c770f191a2e25169dc88d00195381896dc0a81e9
SRINIVAS BN <srinidell2002@gmail.com>
2025-06-08 13:12:25 +0530

    Initial commit - frontend files only

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>
+      <h>hello</h>
+   
+    </div>
+
+
+  </React.StrictMode>
+);
+
+
